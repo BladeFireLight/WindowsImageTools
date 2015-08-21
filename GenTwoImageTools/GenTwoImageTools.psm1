fb9268c1b8d93aa73e7a564dd9afe6fab0d9eb8c
David Jones <BladeFireLight@outlook.com>
2015-08-20 23:28:42 -0500

    Added function to resolve non-existing relative path added test file

--- GenTwoImageTools/GenTwoImageTools.psm1
+++ GenTwoImageTools/GenTwoImageTools.psm1
@@ -2,3 +2,46 @@
 . $PSScriptRoot\Functions\Convert-Wim2GenTwoVhdx.ps1
 . $PSScriptRoot\Functions\Initialize-GenTwoBootDisk.ps1
 . $PSScriptRoot\Functions\Set-GenTwoBootDiskFromWim.ps1
+
+function get-AbsoluteFilePath
+{
+    <#
+            .Synopsis
+            Get Absolute path from relative path
+            .DESCRIPTION
+            Takes a relative path like .\file.txt and returns the full path.
+            Parent folder must exist, but target file does not.
+            The target file does not have to exist, but the parent folder must exist
+            .EXAMPLE
+            $path = Get-AbsoluteFilePath -Path .\file.txt
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        # Path to file
+        [Parameter(Mandatory = $true,
+                ValueFromPipeline = $true,
+        Position = 0)]
+        $Path
+    )
+
+    if (-not (Test-Path $Path))
+    {
+        if (Test-Path (Split-Path -Path $Path -Parent ))
+        {
+            $Parent = Resolve-Path (Split-Path -Path $Path -Parent )
+            $Leaf = Split-Path -Path $Path -Leaf
+            $Path = "$Parent" + "\$Leaf"
+        }
+        else 
+        {
+            throw "Parent [$(Split-Path -Path $Path -Parent)] does not exist"
+        }
+    }
+    else 
+    {
+        $Path = Resolve-Path $Path
+    }
+    return $Path
+}
---
+++ test.ps1
@@ -0,0 +1,16 @@
+﻿if (get-module GenTwoImageTools) { Remove-Module GenTwoImageTools}
+
+import-module C:\GitRepo\GenTwoImageTools\GenTwoImageTools
+
+cd g:\temp
+Initialize-GenTwoBootDisk -Path .\temp1.vhdx -Dynamic -Recovery -Verbose
+Set-GenTwoBootDiskFromWim -Path .\temp1.vhdx -WIMPath .\test.wim -Index 1 -Verbose -Confirm:$false
+
+#Convert-Wim2GenTwoVhdx -Path g:\temp\test1.vhdx -WIMPath g:\temp\test.wim -Dynamic -Index 1 -Size 50GB -Verbose
+
+<# todo
+* fix confirm
+* replace invoke expression
+* add example
+* add about_
+#>
